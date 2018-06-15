4e21ca1944cd9d9b76735d91d5b3fc0c15db007d
Alex Zhang <alexgzhang92@gmail.com>
2018-06-15 03:31:28 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
-      
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :description
     end
   end
 end
